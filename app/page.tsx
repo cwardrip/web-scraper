f8dc09af88ae06fd25e3364a0559df517448ecd8
Chris Wardrip <cwardrip15@gmail.com>
2024-06-05 23:30:10 -0400

    implemented separate product infro card with values

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,12 @@
 import HeroCarousel from '@/components/HeroCarousel'
 import Searchbar from '@/components/Searchbar'
 import Image from 'next/image'
-import React from 'react'
+import { getAllProducts } from '@/lib/actions'
+import ProductCard from '@/components/ProductCard'
 
-const Home = () => {
+const Home = async () => {
+  const allProducts = await getAllProducts();
+
   return (
     <>
       <section className='px-6 md:px-20 py-24'>
@@ -21,7 +24,7 @@ const Home = () => {
 
             <h1 className='head-text'>
               Unleash the Power of
-              <span className='text-primary'> PriseWise</span>
+              <span className='text-primary'> PriceWise</span>
             </h1>
 
             <p className='mt-6'>
@@ -38,8 +41,8 @@ const Home = () => {
       <section className='trending-section'>
         <h2 className='section-text'>Trending</h2>
         <div className='flex flex-wrap gap-x-8 gap-y-16'>
-          {['Apple Iphone 15', 'Book', 'Sneakers'].map((product) => (
-            <div>{product}</div>
+          {allProducts?.map((product) => (
+            <ProductCard key={product._id} product={product} />
           ))}
         </div>
       </section>

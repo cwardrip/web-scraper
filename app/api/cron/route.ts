907d57e752c86e9d0e7a70a211ce193f254f3c46
Chris Wardrip <cwardrip15@gmail.com>
2024-06-09 13:08:28 -0400

    updated maxDuration to 60 seconds

--- app/api/cron/route.ts
+++ app/api/cron/route.ts
@@ -6,7 +6,7 @@ import { getAveragePrice, getEmailNotifType, getHighestPrice, getLowestPrice } f
 import { NextResponse } from "next/server";
 
 //300 seconds
-export const maxDuration = 300;
+export const maxDuration = 60;
 
 export const dynamic = 'force-dynamic';
 export const revalidate = 0;
